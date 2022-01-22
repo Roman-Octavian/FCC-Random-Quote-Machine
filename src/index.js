8042541fbc9216689243ebe52d5b0203aa3d6d0d
Octavian-Roman <work.octavian@gmail.com>
2022-01-22 23:06:08 +0100

    Basic components + fulfilled user stories
    React components for everything that is essential have been added. The user stories required to pass the fCC project requirements have been fulfilled. Some temporary styling has been implemented to facilitate visibility of the different components. Fonts Awesome Icons have been added.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,76 @@ import ReactDOM from "react-dom";
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+class QuoteBox extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+        <wrapper id="quote-box" className="text-center">
+            <Text />
+            <Author />
+            <NewQuoteButton />
+            <TweetQuote />
+        </wrapper>
+        );
+    }
+}
+
+class Text extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1 id="text" className="text-primary">A quote goes here</h1>
+        );
+    }
+}
+
+class Author extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h2 id="author">An author goes here</h2>
+        )
+    }
+}
+
+class NewQuoteButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <button type="button" id="new-quote" className="btn btn-primary">New Quote</button>
+        )
+    }
+}
+
+const tweetIcon = (
+    <i id="fas fa-thumbs-up"></i>
+)
+class TweetQuote extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <a href="https://twitter.com/intent/tweet" id="tweet-quote">
+                <button type="button" className="btn btn-outline-primary">
+                    <i class="fab fa-twitter"></i> Tweet
+                </button>
+            </a>
+        )
+    }
+}
+
 ReactDOM.render(
     <div className="container-fluid">
-        <h1 className="text-primary text-center">Hello ReactJS!</h1>
+        <QuoteBox />
     </div>,
     document.getElementById("root")
 )
